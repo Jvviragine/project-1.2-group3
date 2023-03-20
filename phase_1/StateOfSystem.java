079b1cb57be66114af9de277ec6cbcbc118525fa
ulasawczuk <sawczuk.ula@gmail.com>
2023-03-20 15:54:30 +0100

    basic class for vector and state of solar system

---
+++ phase_1/StateOfSystem.java
@@ -0,0 +1,75 @@
+package phase_1;
+
+import java.util.ArrayList;
+
+public class StateOfSystem {
+
+    ArrayList<Vector> positions = new ArrayList<>();
+    ArrayList<Vector> velocities = new ArrayList<>();
+    double timeOfState;
+
+    //constructor
+    public StateOfSystem(double t0, ArrayList<Vector> positions, ArrayList<Vector> velcities) {
+        this.positions = positions;
+        this.velocities = velcities;
+        timeOfState = t0;
+    }
+
+    public ArrayList<Vector> getPositions(){
+        return positions;
+    }
+
+    public ArrayList<Vector> getVelocities(){
+        return velocities;
+    }
+
+    public double getTimeOfState(){
+        return timeOfState;
+    }
+
+    public void setTime(double time){
+        timeOfState = time;
+    }
+
+    public void setSingleVelocity(int id, Vector newVelocity){
+        ArrayList<Vector> newVelocities = new ArrayList<>();
+        for(int i = 0; i<velocities.size(); i++){
+            if(i == id){
+                newVelocities.add(newVelocity);
+            }else{
+                newVelocities.add(velocities.get(i));
+            }
+        }
+        velocities = newVelocities;
+    }
+
+    public void setSinglePosition(int id, Vector newPosition){
+        ArrayList<Vector> newPositions = new ArrayList<>();
+        for(int i = 0; i<positions.size(); i++){
+            if(i == id){
+                newPositions.add(newPosition);
+            }else{
+                newPositions.add(positions.get(i));
+            }
+        }
+        positions = newPositions;
+    }
+
+    public String toString(){//to print the positions and velocities
+        StringBuilder str = new StringBuilder();
+        str.append("positions: ");
+        for(int i = 0; i<positions.size(); i++){
+            str.append(positions.get(i).toString());
+            str.append(", ");
+        }
+
+        str.append("\n");
+        str.append("velocities: ");
+        for(int i = 0; i<velocities.size(); i++){
+            str.append(velocities.get(i).toString());
+            str.append(", ");
+        }
+        return str.toString();
+    }
+
+}
---
+++ phase_1/Vector.java
@@ -0,0 +1,78 @@
+package phase_1;
+
+public class Vector{
+    private double x;
+    private double y;
+    private double z;
+
+    //constructor for vector with x,y,z
+    public Vector(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    //constructor of empty vector
+    public Vector(){
+
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getZ(){
+        return z;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public void setX(double x){
+        this.x = x;
+    }
+
+    public void setY(double y){
+        this.y = y;
+    }
+
+    public void setZ(double z){
+        this.z = z;
+    }
+
+    public Vector add(Vector other){
+        Vector v = new Vector();
+        v.setX(x + other.getX());
+        v.setY(y + other.getY());
+        v.setZ(z + other.getZ());
+        return v;
+    }
+
+    public Vector sub(Vector other){
+        Vector v = new Vector();
+        v.setX(x - other.getX());
+        v.setY(y - other.getY());
+        v.setZ(z - other.getZ());
+        return v;
+    }
+
+    public Vector multi(double s){
+        Vector v = new Vector();
+        v.setX(x*s);
+        v.setY(y*s);
+        v.setZ(z*s);
+        return v;
+    }
+
+    //computes length of a vector
+    public double len(){
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+    }
+
+    //computes euclidean distance between vectors
+    public double dist(Vector other){
+        return Math.sqrt(Math.pow(x - other.getX(), 2) + Math.pow(y - other.getY(), 2) + Math.pow(z - other.getZ(), 2));
+    }
+
+}
