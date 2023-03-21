41b603ad23ce330d8d5ef6f02e7045663178b373
tamar <115258119+tamartrbl@users.noreply.github.com>
2023-03-21 09:39:32 +0100

    Update CelestialBody.java

--- phase_1/CelestialBody.java
+++ phase_1/CelestialBody.java
@@ -1,17 +1,39 @@
 package phase_1;
 public class CelestialBody{
+    Vector position;
+    Vector velocity;
     double mass;
-    Vector initialposition;
-    Vector initialvelocity;
+    double radius;
 
-    public CelestialBody(Vector initialposition,Vector initalvelocity,double mass){
-        this.initialposition=initialposition;
-        this.initialvelocity=initalvelocity;
+    public CelestialBody(Vector initialposition,Vector initialvelocity,double mass,double radius){
+        this.position=initialposition;
+        this.velocity=initialvelocity;
         this.mass=mass;
+        this.radius=radius;
     }
     
     public double getMass(){
         return mass;
+    }
+
+    public void setPosition(Vector position){
+        this.position=position;
+    }
+
+    public Vector getPosition(){
+        return position;
+    }
+
+    public void setVelocity(Vector velocity){
+        this.velocity=velocity;
+    }
+
+    public Vector getVelocity(){
+        return velocity;
+    }
+
+    public double getRadius(){
+        return radius;
     }
 
 }
