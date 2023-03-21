383894d1762e10fb6d25ae493a51d99b0e6c9a89
tamar <115258119+tamartrbl@users.noreply.github.com>
2023-03-21 09:21:18 +0100

    Create CelestialBody.java
    celestial body class

---
+++ phase_1/CelestialBody.java
@@ -0,0 +1,19 @@
+package phase_1;
+public class CelestialBody{
+    double mass;
+    Vector initialposition;
+    Vector initialvelocity;
+
+    public CelestialBody(Vector initialposition,Vector initalvelocity,double mass){
+        this.initialposition=initialposition;
+        this.initialvelocity=initalvelocity;
+        this.mass=mass;
+    }
+    public void setMass(double mass){
+        this.mass=mass;
+    }
+    public double getMass(){
+        return mass;
+    }
+
+}
