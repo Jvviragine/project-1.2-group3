a52c519cad865c10dd24af03ef2a3bec1fee157e
wik <ritwik2504@gmail.com>
2023-03-23 19:34:52 +0100

    Update SolarSystemViewer.java

--- phase_1/SolarSystemViewer.java
+++ phase_1/SolarSystemViewer.java
@@ -19,7 +19,7 @@ public class SolarSystemViewer
     public static double s = (celestialObjects.getMaxDistanceFromSun(1253801723.95465, -760453007.810989, -36697431.1565206));
     public static double m = (celestialObjects.getMaxDistanceFromSun(-159116303.422552, 189235671.561057, 7870476.08522969));
     public static double sum = s + m;
-    public static final double scale = Math.ceil(sum / 100000000) * 100000000 / (h-200);
+    public static final double scale = Math.ceil((sum / 100000000) * (100000000 )/ (h-200));
     public static double[][] rocketLaunch = new double[2][3];
 
     public static void main(String[] args) {
