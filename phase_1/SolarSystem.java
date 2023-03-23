4da13b886d45ed84868601f9d559a7145cb86edb
AinstvonHowling <g.latimer@student.maastrichtuniversity.nl>
2023-03-23 13:03:11 +0100

    Temporary Changes, not sure if bueno

--- phase_1/SolarSystem.java
+++ phase_1/SolarSystem.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.*;
 import java.security.spec.EllipticCurve;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.awt.image.BufferedImage;
@@ -54,8 +55,6 @@ public class SolarSystem extends JPanel implements ActionListener
 
         int sunX = (int) ((celestialObjects.mars.getDistanceFromSun()/SolarSystemViewer.scale) + 70);
         int sunY = (int) SolarSystemViewer.h/3;
-        // int rocketX = (int) Rocket.getXPos();
-        // int rocketY = (int) Rocket.getYPos();
 
         //Generate the sun
         try 
@@ -87,67 +86,146 @@ public class SolarSystem extends JPanel implements ActionListener
         int adjustW = 0;
         int offset = 0;
 
-        for(int i = 0; i < image.length; i++)
-        {
-            t = i+1;
-            celestialObjects r = celestialObjects.getBody(t);
-            double ax = r.x1/SolarSystemViewer.scale;
-            double ay = r.x2/SolarSystemViewer.scale;
+        double radiusTitan = 2.575;
+        double c = 0.1*radiusTitan;
+        
+        // Create a new Solar System
 
-            try 
-            {
-                String path = image[i];
-                trial = ImageIO.read(getClass().getResource(path));
-                switch(i) {
-                    case 0, 1: 
-                        offset = 0;
-                        adjustH = SolarSystemViewer.h/38;
-                        adjustW = adjustH;
-                        break;
-                    case 2: 
-                        offset = SolarSystemViewer.h/96;
-                        adjustH = SolarSystemViewer.h/96;
-                        adjustW = adjustH;
-                        break;
-                    case 3: 
-                        offset = 0;
-                        adjustH = SolarSystemViewer.h/48;
-                        adjustW = adjustH;
-                        break;
-                    case 4: 
-                        offset = 0;
-                        adjustH = SolarSystemViewer.h/28;
-                        adjustW = adjustH;
-                        break;
-                    case 5: 
-                        offset = 0;
-                        adjustH = SolarSystemViewer.h/28;
-                        adjustW = SolarSystemViewer.h/16;
-                        break;
-                    case 6: 
-                        offset = 0;
-                        adjustH = SolarSystemViewer.h/96;
-                        adjustW = adjustH;
-                        break;
+        // Create the Sun
+        Vector sunInitialPositions = new Vector(0, 0, 0);
+        Vector sunInitialVelocity = new Vector(0, 0, 0);
+        CelestialBody sun = new CelestialBody(sunInitialPositions, sunInitialVelocity, 1.9885*Math.pow(10, 30), "Sun");
+
+        // Create Venus
+        Vector venusInitialPositions = new Vector((-28216773.9426889), (103994008.541512), (3012326.64296788));
+        Vector venusInitialVelocity = new Vector((-34.0236737066136), (-8.96521274688838), (1.84061735279188));
+        CelestialBody venus = new CelestialBody(venusInitialPositions, venusInitialVelocity, 48.685*Math.pow(10,23), "Venus");
+
+        // Create the Earth
+        Vector earthInitialPositions = new Vector((-148186906.893642), (-27823158.5715694), (33746.8987977113));
+        Vector earthInitialVelocity = new Vector((5.05251577575409), (-29.3926687625899), (0.00170974277401292));
+        CelestialBody earth = new CelestialBody(earthInitialPositions, earthInitialVelocity, 5.97219*Math.pow(10,24), "Earth");
+
+        // Create the Moon
+        Vector moonInitialPositions = new Vector((-148458048.395164), (-27524868.1841142), (70233.6499287411));
+        Vector moonInitialVelocity = new Vector((4.34032634654904), (-30.0480834180741), (-0.0116103535014229));
+        CelestialBody moon = new CelestialBody(moonInitialPositions, moonInitialVelocity, 7.349*Math.pow(10,22), "Moon");
+
+        // Create Mars
+        Vector marsInitialPositions = new Vector((-159116303.422552), (189235671.561057), (7870476.08522969));
+        Vector marsInitialVelocity = new Vector((-17.6954469224752), (-13.4635253412947), (0.152331928200531));
+        CelestialBody mars = new CelestialBody(marsInitialPositions, marsInitialVelocity, 6.4171*Math.pow(10,23) , "Mars");
+
+        // Create Jupiter
+        Vector jupiterInitialPositions = new Vector((692722875.928222), (258560760.813524), (-16570817.7105996));
+        Vector jupiterInitialVelocity = new Vector((-4.71443059866156), (12.8555096964427), (0.0522118126939208));
+        CelestialBody jupiter = new CelestialBody(jupiterInitialPositions, jupiterInitialVelocity, 189818722*Math.pow(10,19), "Jupiter");
+
+        // Create Saturn
+        Vector saturnInitialPositions = new Vector((1253801723.95465), (-760453007.810989), (-36697431.1565206));
+        Vector saturnInitialVelocity = new Vector((4.46781341335014), (8.23989540475628), (-0.320745376969732));
+        CelestialBody saturn = new CelestialBody(saturnInitialPositions, saturnInitialVelocity, 5.6834*Math.pow(10,26), "Saturn");
+
+        // Create Titan
+        Vector titanInitialPositions = new Vector((1254501624.95946), (-761340299.067828), (-36309613.8378104));
+        Vector titanInitialVelocity = new Vector((8.99593229549645), (11.1085713608453), (-2.25130986174761));
+        CelestialBody titan = new CelestialBody(titanInitialPositions, titanInitialVelocity, 13455.3*Math.pow(10,19), "Titan");
+
+        // Create the Probe
+        //Vector probeInitialPosition = new Vector((-148186906.893642), (-27823158.5715694), (33746.8987977113));
+        Vector probeInitialPosition = new Vector(-148186906.893642 + 6700, -27823158.5715694 + 6700, 33746.8987977113 + 6700);
+        Vector probeInitialVelocity = new Vector(0, 0, 0);
+        CelestialBody probe = new CelestialBody(probeInitialPosition, probeInitialVelocity,  50000, "Probe");
+
+        // Collection of Celestial Bodies
+        ArrayList<CelestialBody> celestialBodies = new ArrayList<CelestialBody>();
+        celestialBodies.add(sun);
+        celestialBodies.add(venus);
+        celestialBodies.add(earth);
+        celestialBodies.add(moon);
+        celestialBodies.add(mars);
+        celestialBodies.add(jupiter);
+        celestialBodies.add(saturn);
+        celestialBodies.add(titan);
+        celestialBodies.add(probe);
+
+        int timeStepInSeconds = 60 * 60; // 1 Month of Timestep
+        StateOfSystem solarSystemState3 = new StateOfSystem(timeStepInSeconds, celestialBodies); // Giging it a Time Step of
+        
+        // Create the Object that can Update the State of the Solar System
+        UpdateStateOfSystem solarSystemUpdater3 = new UpdateStateOfSystem(solarSystemState3);
+        solarSystemUpdater3.calculateMissingValuesForT0();
+
+        // Loop controlling how many TimeSteps we are going to take
+        int lastT = (365 * 24) + 6; // Years 10756
+        int currT = 0;
+        while(probe.getPosition().dist(titanInitialPositions) > radiusTitan+c && currT != lastT){
+            solarSystemUpdater3.updateStateOfSolarSystem();
+            currT += 1;
+        }
+
+        for(int j = 1; j < celestialBodies.size(); j++){
+            // for(int i = 1; i < image.length; i++)
+            // {
+                double ax = celestialBodies.get(j).getPosition().getX()/SolarSystemViewer.scale;
+                double ay = celestialBodies.get(j).getPosition().getY()/SolarSystemViewer.scale;
+            
+                try 
+                {
+                    System.out.println(ax);
+                    String path = image[j - 1];
+                    trial = ImageIO.read(getClass().getResource(path));
+                    switch(j - 1) {
+                        case 0, 1: 
+                            offset = 0;
+                            adjustH = SolarSystemViewer.h/38;
+                            adjustW = adjustH;
+                            break;
+                        case 2: 
+                            offset = SolarSystemViewer.h/96;
+                            adjustH = SolarSystemViewer.h/96;
+                            adjustW = adjustH;
+                            break;
+                        case 3: 
+                            offset = 0;
+                            adjustH = SolarSystemViewer.h/48;
+                            adjustW = adjustH;
+                            break;
+                        case 4: 
+                            offset = 0;
+                            adjustH = SolarSystemViewer.h/28;
+                            adjustW = adjustH;
+                            break;
+                        case 5: 
+                            offset = 0;
+                            adjustH = SolarSystemViewer.h/28;
+                            adjustW = SolarSystemViewer.h/16;
+                            break;
+                        case 6: 
+                            offset = 0;
+                            adjustH = SolarSystemViewer.h/96;
+                            adjustW = adjustH;
+                            break;
+                        case 7:
+                            offset = 0;
+                            adjustH = SolarSystemViewer.h/96;
+                            adjustW = SolarSystemViewer.h/16;
+
+                    }
+
+                    g2D.drawImage(trial, (int) ((sunX+SolarSystemViewer.h/30)+ax)-15-offset,(int) ((sunY+SolarSystemViewer.h/30)-ay)-15-offset , adjustW, adjustH, observer);
+                    // g2D.drawString(label[i], (int) (sunX+ax)-10, (int) (sunY-ay)-10);
+
+                    // g2D.drawImage(trial, (int) Math.round(-1*((SolarSystemViewer.h/2 - 3)-ax)+(SolarSystemViewer.h)) ,(int) Math.round((SolarSystemViewer.h/2 - 3)-ay) , 7, 7, observer);
+                    // g2D.drawString(label[i], (int) Math.round(-1*((SolarSystemViewer.h/2 + 10)-ax)+(SolarSystemViewer.h)) , (int) Math.round((SolarSystemViewer.h/2 - 5)-ay));
+                } 
+                catch (IOException e) 
+                {
+                    e.printStackTrace();
                 }
-                
-            //     else{
-            //       offset = 0;
-            //       g2D.drawImage(trial, rocketX, rocketY, SolarSystemViewer.h/96, SolarSystemViewer.h/96, observer);
-            // }
-
-                g2D.drawImage(trial, (int) ((sunX+SolarSystemViewer.h/30)+ax)-15-offset,(int) ((sunY+SolarSystemViewer.h/30)-ay)-15-offset , adjustW, adjustH, observer);
-                // g2D.drawString(label[i], (int) (sunX+ax)-10, (int) (sunY-ay)-10);
-
-                // g2D.drawImage(trial, (int) Math.round(-1*((SolarSystemViewer.h/2 - 3)-ax)+(SolarSystemViewer.h)) ,(int) Math.round((SolarSystemViewer.h/2 - 3)-ay) , 7, 7, observer);
-                // g2D.drawString(label[i], (int) Math.round(-1*((SolarSystemViewer.h/2 + 10)-ax)+(SolarSystemViewer.h)) , (int) Math.round((SolarSystemViewer.h/2 - 5)-ay));
-            } 
-            catch (IOException e) 
-            {
-                e.printStackTrace();
-            }
-        }  
-    }
+            }  
+        }
+    // }
 
     @Override
     public void actionPerformed(ActionEvent e) 
--- phase_1/SolarSystemViewer.java
+++ phase_1/SolarSystemViewer.java
@@ -1,16 +1,25 @@
 package phase_1;
 
 import java.awt.*;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JRootPane;
+import javax.swing.JTextField;
+import javax.swing.JWindow;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.plaf.metal.MetalLookAndFeel;
 import javax.tools.Tool;
 
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SolarSystemViewer {
+public class SolarSystemViewer{
     public static Toolkit tk = Toolkit.getDefaultToolkit();
     public static Dimension screenSize = tk.getScreenSize();
     public static int h = screenSize.height;
@@ -26,6 +35,69 @@ public class SolarSystemViewer {
     public static final double scale = Math.ceil(sum / 100000000) * 100000000 / (h-200);
 
     public static void main(String[] args) {
+
+        // JFrame initialConditions = new JFrame();
+        // initialConditions.setTitle("Starting Conditions");
+        // JPanel format = new JPanel();
+        // format.setLayout(new GridLayout(1, 7));
+        // JPanel x1 = new JPanel();
+        // JPanel x2 = new JPanel();
+        // JPanel x3 = new JPanel();
+        // JPanel v1 = new JPanel();
+        // JPanel v2 = new JPanel();
+        // JPanel v3 = new JPanel();
+        // JButton confirm = new JButton();
+        // JLabel x11 = new JLabel("x0");
+        // JLabel x21 = new JLabel("y0");
+        // JLabel x31 = new JLabel("z0");
+        // JLabel v11 = new JLabel("v1");
+        // JLabel v21 = new JLabel("v2");
+        // JLabel v31 = new JLabel("v3");
+        // JTextField x12 = new JTextField();
+        // JTextField x22 = new JTextField();
+        // JTextField x32 = new JTextField();
+        // JTextField v12 = new JTextField();
+        // JTextField v22 = new JTextField();
+        // JTextField v32 = new JTextField();
+        // initialConditions.add(format);
+        // format.add(x1);
+        // format.add(x2);
+        // format.add(x3);
+        // format.add(v1);
+        // format.add(v2);
+        // format.add(v3);
+        // format.add(confirm);
+        // x1.add(x11);
+        // x1.add(x12);
+        // x2.add(x21);
+        // x2.add(x22);
+        // x3.add(x31);
+        // x3.add(x32);
+        // v1.add(v11);
+        // v1.add(v12);
+        // v2.add(v21);
+        // v2.add(v22);
+        // v3.add(v31);
+        // v3.add(v32);
+        // format.setVisible(true);
+        // try {
+        //     double input1 = Double.parseDouble(x12.getText());
+        //     double input2 = Double.parseDouble(x22.getText());
+        //     double input3 = Double.parseDouble(x32.getText());
+        //     double input4 = Double.parseDouble(v12.getText());
+        //     double input5 = Double.parseDouble(v22.getText());
+        //     double input6 = Double.parseDouble(v32.getText());
+        // } catch (NumberFormatException e) {
+        //     // TODO: handle exception
+        // } 
+        // confirm.addActionListener(new ActionListener() {
+        //     @Override
+        //     public void actionPerformed(ActionEvent e) {
+        //         if(input1 != 0 && input2 != 0 && input3 != 0 && input4 != 0 && input5 != 0 && input6 != 0)
+        //     format.setVisible(false);
+        //     }
+        // });
+     
 
         JFrame solarSystemFrame = new JFrame();
         solarSystemFrame.setTitle("Solar System Visualization");
